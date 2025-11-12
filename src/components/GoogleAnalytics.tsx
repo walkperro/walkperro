484c30cfa55b3e45ed37c8f9d8be5fafe6259095
walkperro <johnsalsberry1980@gmail.com>
2025-11-11 23:17:28 -0600

    fix: add GoogleAnalytics component and ensure @ alias to src

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Script from "next/script";
+
+export default function GoogleAnalytics() {
+  const id = process.env.NEXT_PUBLIC_GA_ID;
+  if (!id) return null;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { anonymize_ip: true });
+        `}
+      </Script>
+    </>
+  );
+}
